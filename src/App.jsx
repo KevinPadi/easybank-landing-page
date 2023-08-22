cde38741ced94677efa190f092ff9297740f203d
Kevin <kevpadilla01@gmail.com>
2023-08-22 18:31:19 -0300

    feat: implement responsive design for articles

--- src/App.jsx
+++ src/App.jsx
@@ -31,15 +31,15 @@ function App () {
         </div>
       </section>
       {/* Cards Container */}
-      <section className='bg-LightGrayish md:px-64 md:py-28'>
-        <div className='md:space-y-5'>
-          <h2 className='text-DarkBlue md:text-4xl'>Why choose Easybank?</h2>
-          <p className='text-GrayishBlue md:text-lg'>
+      <section className='space-y-10 px-6 py-14 bg-LightGrayish md:px-64 md:py-28 md:space-y-16'>
+        <div className='text-center space-y-5 md:space-y-5 md:w-[700px] md:text-left'>
+          <h2 className='text-DarkBlue text-4xl '>Why choose Easybank?</h2>
+          <p className='text-GrayishBlue md:text-lg md:whitespace-break-spaces'>
             We leverage Open Banking to turn your bank account into your financial hub.
             Control your finances like never before.
           </p>
         </div>
-        <div className='md:flex md:mt-44 md:items-center md:justify-center md:gap-8'>
+        <div className='space-y-10 md:flex md:mt-40 md:items-center md:justify-center md:gap-8 md:space-y-0'>
           <Cards title='Online Banking' text='Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.' src={iconOnline} />
           <Cards title='Simple Budgeting' text="See exactly where your money goes each month. Receive notification when you're close to hitting your limits " src={iconBudgeting} />
           <Cards title='Fast Onboarding' text="We don't do branches. Open your account in minutes online and start taking control of your finances right away" src={iconOnboarding} />
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Cards ({ src, title, text }) {
   return (
-    <article className='md:w-[260px] md:space-y-6'>
-      <div>
+    <article className='text-center space-y-6 md:w-[260px] md:space-y-6 md:text-left'>
+      <div className='flex justify-center md:justify-start'>
         <img src={src} alt='Card Icon' />
       </div>
-      <h2 className='md:text-2xl text-DarkBlue'>{title}</h2>
+      <h2 className='text-2xl text-DarkBlue'>{title}</h2>
       <p className='text-GrayishBlue'>{text}</p>
     </article>
   )
